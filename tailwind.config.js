7340ada6bf5eec6b93e19844b956595345001f88
xkalf0710 <mnymsuren37@gmail.com>
2025-07-02 17:31:48 +0800

    ADD

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        segoe: ['"Segoe UI"', 'sans-serif'],
+        authenia: ['Authenia', 'cursive'],
+      },
+    },
+  },
+}
